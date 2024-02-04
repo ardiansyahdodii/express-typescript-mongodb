991fefc66b8ed7f713a1bb72ac1e56bbd215fa3d
ardiansyahdodii <dodyn890@gmail.com>
2024-02-04 22:56:10 +0700

    Routing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from 'express';
+import { routes } from './routes';
 
 const app: Application = express()
 const port: number = 9000
 
-app.get('/', (req:Request, res:Response) => {
-    res.status(200).json({ message: 'Express Typescript' })
-})
+// app.get('/', (req:Request, res:Response) => {
+//     res.status(200).json({ message: 'Express Typescript' })
+// })
+routes(app)
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
---
+++ src/routes/product.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+export const ProductRouter: Router = Router()
+
+ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json(
+        {
+            status: true,
+            statusCode: 200,
+            data: [
+                {
+                    id: 1,
+                    name: 'Iphone 15 Pro',
+                    price: 20000000,
+                },
+                {
+                    id: 2,
+                    name: 'Iphone 11',
+                    price: 6500000,
+                }
+            ]
+        })
+})
